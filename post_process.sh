77bb6026575396439332bde76c12393ad70f0ef5
Novarizark <novarizark@gmail.com>
2018-02-07 08:13:50 +0800

    20180207

--- post_process.sh
+++ post_process.sh
@@ -14,6 +14,9 @@ mkdir $MAP_DIR
 
 cd $WORKDIR/ncl
 ncl 180124-plot-T2m-RH-timeseries.ncl 
+ncl 180205-plot-snow-cover.ncl
+ncl 180205-plot-precip.ncl
+
 cd $WORKDIR/fig
 
 
@@ -21,8 +24,24 @@ cd $WORKDIR/fig
 convert T2m-home.png -bordercolor white -trim T2m-home.png
 
 
+#for((I=0;I<=31;I++))
+#do
+#    TF=$(expr $I \* 3)
+#    TFSTMP=`printf "%.3d" $TF`
+#    convert snow-cover_${TFSTMP}H.png -bordercolor white -trim snow-cover_${TFSTMP}H.png
+#done
+
+# convert gif
+convert -delay 100 snow-cover_* snow-cover.gif
+convert -delay 100 precip_* precip.gif
+
 # Move realtime figures to repo
 cp T2m-home.png $TS_DIR 
+cp snow* $MAP_DIR
+cp precip* $MAP_DIR
+
+rm -f snow-cover_*
+rm -f precip_*
 
 cd $WORKDIR
 sh gitfresh.sh 
