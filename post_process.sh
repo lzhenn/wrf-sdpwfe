2e96ca6c14c80de4a53bf278c122b8d9a80950c9
Novarizark <novarizark@gmail.com>
2018-02-10 19:39:17 +0800

    20180210

--- post_process.sh
+++ post_process.sh
@@ -12,6 +12,10 @@ mkdir $TS_DIR
 mkdir $MAP_DIR
 
 
+#--------------------------------------------------------------
+#       First Phase: Core D03 Map and Home Timeseries
+#--------------------------------------------------------------
+
 cd $WORKDIR/ncl
 ncl 180124-plot-T2m-RH-timeseries.ncl 
 ncl 180205-plot-snow-cover.ncl
@@ -51,3 +55,65 @@ mv Wind10m_* $MAP_DIR
 
 cd $WORKDIR
 sh gitfresh.sh 
+
+#--------------------------------------------------------------
+#                 Second Phase: D01 D02 Maps
+#--------------------------------------------------------------
+
+cd $WORKDIR/ncl
+ncl 180210-plot-d02-precip.ncl
+ncl 180210-plot-d02-snow-cover.ncl
+ncl 180210-plot-d02-T2m.ncl
+ncl 180210-plot-d02-RH2m.ncl
+ncl 180210-plot-d02-UV10m.ncl
+
+ncl 180210-plot-d01-precip.ncl
+ncl 180210-plot-d01-snow-cover.ncl
+ncl 180210-plot-d01-T2m.ncl
+ncl 180210-plot-d01-RH2m.ncl
+ncl 180210-plot-d01-UV10m.ncl
+
+
+cd $WORKDIR/fig
+
+
+
+#for((I=0;I<=31;I++))
+#do
+#    TF=$(expr $I \* 3)
+#    TFSTMP=`printf "%.3d" $TF`
+#    convert snow-cover_${TFSTMP}H.png -bordercolor white -trim snow-cover_${TFSTMP}H.png
+#done
+
+# convert gif
+convert -delay 100 d02_snow-cover_* d02_snow-cover.gif
+convert -delay 100 d02_precip_* d02_precip.gif
+convert -delay 100 d02_T2m_* d02_T2m.gif
+convert -delay 100 d02_RH2m_* d02_RH2m.gif
+convert -delay 100 d02_Wind10m_* d02_Wind10m.gif
+
+convert -delay 100 d01_snow-cover_* d01_snow-cover.gif
+convert -delay 100 d01_precip_* d01_precip.gif
+convert -delay 100 d01_T2m_* d01_T2m.gif
+convert -delay 100 d01_RH2m_* d01_RH2m.gif
+convert -delay 100 d01_Wind10m_* d01_Wind10m.gif
+
+
+# Move realtime figures to repo
+mv d02_snow-cover_* $MAP_DIR
+mv d02_precip_* $MAP_DIR
+mv d02_T2m_* $MAP_DIR
+mv d02_RH2m_* $MAP_DIR
+mv d02_Wind10m_* $MAP_DIR
+
+mv d01_snow-cover_* $MAP_DIR
+mv d01_precip_* $MAP_DIR
+mv d01_T2m_* $MAP_DIR
+mv d01_RH2m_* $MAP_DIR
+mv d01_Wind10m_* $MAP_DIR
+
+
+cd $WORKDIR
+sh gitfresh.sh 
+
+
